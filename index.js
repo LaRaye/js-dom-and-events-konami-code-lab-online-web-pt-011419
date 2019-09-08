134ca6641a55ebe36b549d66e07a594184f473aa
LaRaye Johnson <lnj2105@gmail.com>
2019-09-08 19:42:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let i = 0;
 
-function init(e) {
+code.addEventListener (function init(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[i]) {
