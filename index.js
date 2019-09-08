1fc36285cae251de05315e0beda5046eb895e7da
LaRaye Johnson <lnj2105@gmail.com>
2019-09-08 19:48:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let i = 0;
+function init() {
 
-code.addEventListener ('keydown', function init(e) {
-  const key = parseInt(e.detail || e.which);
+  let i = 0;
 
-  if (key === code[i]) {
-    i++;
-    
-    if (i === code.length) {
-      alert("Whoo!");
+  document.body.addEventListener ('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+  
+    if (key === code[i]) {
+      i++;
       
-      i = 0;
-    } else {
-      i = 0;
+      if (i === code.length) {
+        alert("Whoo!");
+        i = 0;
+  
+      } else {
+        i = 0;
+      }
     }
-  }
-});
+  });
+}
